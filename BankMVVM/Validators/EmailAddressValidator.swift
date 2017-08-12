17b915ea8aedb76a4de84d9656cf03861b83c947
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-08-12 21:35:32 +0300

    Added EmailAddressValidatorTests class for testing EmailAddressValidator class.

---
+++ BankMVVM/Validators/EmailAddressValidator.swift
@@ -0,0 +1,46 @@
+//
+//  EmailAddressValidator.swift
+//  BankMVVM
+//
+//  Created by Fomin Mykola on 8/12/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import Foundation
+
+class EmailAddressValidator: NSObject {
+    
+    func validate(_ value: String) -> Bool {
+        if value.characters.count < 6 {
+            return false
+        }
+        
+        let whitespace = Set(" ".characters)
+        let isContainingWhiteSpace = value.characters.filter({ whitespace.contains($0) })
+        if isContainingWhiteSpace.count > 0 {
+            return false
+        }
+        
+        let numbers = Set(" ".characters)
+        let isContainingNumbers = value.characters.filter({ numbers.contains($0) })
+        if isContainingNumbers.count > 0 {
+            return false
+        }
+        
+        let specialCharacters = Set("+,!#$%^&*();\\/|<>\"".characters)
+        let isContainingSpecialCharacters = value.characters.filter({ specialCharacters.contains($0) })
+        if isContainingSpecialCharacters.count > 0 {
+            return false
+        }
+        
+        guard let regexValidator = try? NSRegularExpression(pattern: "([A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}]))", options: .caseInsensitive) else { return false }
+        
+        if regexValidator.numberOfMatches(in: value, options: NSRegularExpression.MatchingOptions.reportCompletion, range: NSMakeRange(0, value.characters.count)) > 0 {
+            return true
+        }
+        
+        return false
+        
+    }
+    
+}
---
+++ BankMVVMTests/EmailAddressValidatorTests.swift
@@ -0,0 +1,96 @@
+//
+//  EmailAddressValidatorTests.swift
+//  BankMVVMTests
+//
+//  Created by Fomin Mykola on 8/12/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import XCTest
+@testable import BankMVVM
+
+class EmailAddressValidatorTests: XCTestCase {
+    
+    fileprivate let emptyString = ""
+    fileprivate let validEmailAddress1 = "a@b.com"
+    fileprivate let validEmailAddress2 = "a@b.co.uk"
+    fileprivate let validEmailAddress3 = "a@b.io"
+    fileprivate let validEmailAddress4 = "andrew.shaw@byteowl.io"
+    fileprivate let invalidEmailAddress1 = "ab.com"
+    fileprivate let invalidEmailAddress2 = "abcom"
+    fileprivate let invalidEmailAddress3 = "a@b@com"
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+   
+}
+
+//MARK: Empty string validation
+extension EmailAddressValidatorTests {
+    
+    func testValidate_EmptyString_ReturnsFalse() {
+        let validator = EmailAddressValidator()
+        XCTAssertFalse(validator.validate(emptyString), "string can not be empty.")
+    }
+    
+}
+
+//MARK: Invalid email-address
+extension EmailAddressValidatorTests {
+    
+    func testValidate_InvalidEmailAddress1_ReturnsFalse() {
+        let validator = EmailAddressValidator()
+        XCTAssertFalse(validator.validate(invalidEmailAddress1), "\(invalidEmailAddress1) is not a valid e-mail address.")
+    }
+    
+    func testValidate_InvalidEmailAddress2_ReturnsFalse() {
+        let validator = EmailAddressValidator()
+        XCTAssertFalse(validator.validate(invalidEmailAddress2), "\(invalidEmailAddress2) is not a valid e-mail address.")
+    }
+    
+    func testValidate_InvalidEmailAddress3_ReturnsFalse() {
+        let validator = EmailAddressValidator()
+        XCTAssertFalse(validator.validate(invalidEmailAddress3), "\(invalidEmailAddress3) is not a valid e-mail address.")
+    }
+    
+    func testValidate_InvalidEmailAddress4_ReturnsFalse() {
+        let validator = EmailAddressValidator()
+        XCTAssertFalse(validator.validate(validEmailAddress4), "\(validEmailAddress4) is not a valid e-mail address.")
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
